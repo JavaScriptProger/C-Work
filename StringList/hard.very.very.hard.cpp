b58ef04c177779ccfe74e3bf6b5effc50957689e
ArtemY <35269498+JavaScriptProger@users.noreply.github.com>
2019-03-05 18:45:52 +0300

    Teperi c moim sposobom =)

--- StringList/hard.very.very.hard.cpp
+++ StringList/hard.very.very.hard.cpp
@@ -26,50 +26,59 @@ void ferststep(S *m, int &count)
 int main ()
 {
 	S m[30];
- ifstream fin("Z:\in.txt", std::ios::in);
- int count=-1;
-while (!fin.eof())
- {
- char ch = fin.get();
- int flag=0;
- for(int i=0; i<=count;i++)
- {
-	 if(ch==m[i].b)
-	 {
-		 m[i].k++;
-		 flag=1;
-		 break;
-	 }
- }
- if(flag==0)
- {
-	 count++;
-	 m[count].b=ch;
-	 m[count].k=1;
- }
-}
-count--;
-for(int i=0; i<=count; i++)
-{
-	cout<<m[i].b<<" "<<m[i].k<<endl;
-	m[i].flag=1;
-	m[i].L=NULL;
-	m[i].R=NULL;
-}
-cout<<"-------------"<<endl;
+	ifstream fin("Z:\in.txt", std::ios::in);
+	int count=-1;
+	while (!fin.eof()) {
+		 char ch = fin.get();
+		 int flag=0;
+		 for(int i=0; i<=count;i++)
+		 {
+			 if(ch==m[i].b)
+			 {
+				 m[i].k++;
+				 flag=1;
+				 break;
+			 }
+		 }
+		 if(flag==0)
+		 {
+			 count++;
+			 m[count].b=ch;
+			 m[count].k=1;
+		 }
+	}
+	count--;
+	for(int i=0; i<=count; i++)
+	{
+		cout<<m[i].b<<" "<<m[i].k<<endl;
+		m[i].flag=1;
+		m[i].L=NULL;
+		m[i].R=NULL;
+	}
+	cout<<"-------------"<<endl;
 
-for(int i=0;i<count;i++)
-	for(int j=0;j<count;j++)
-		if(m[j].k>m[j+1].b)
-		{
-			S tmp;
-			tmp=m[j];
-			m[j]=m[j+1];
-			m[j+1]=tmp;
+	for(int i = 0;i < 15;i++){
+		int num = m[i].k;
+		int pos = i;
+
+		for(int j = 0;j < 15;j++){
+			if(num > m[j].k){
+				int pos1 = j;
+				int v1 = m[j].k;
+
+				m[j].k = num;
+				m[pos].k = v1;
+
+				break;
+			}
 		}
-for(int i=0; i<=count; i++)
-	cout<<m[i].b<<" "<<m[i].k<<endl;
+	}
 
+	for(int i=0; i<=count; i++){
+		cout<<m[i].b<<" "<<m[i].k<<endl;
+	}
 
+	system("pause");
 
+	return 0;
 }
